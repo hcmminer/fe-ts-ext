d9aebd3f1dd9ce439e8399b79fc43a0fa531854b
ban_ga_danh_che <hcmminer@gmail.com>
2024-11-05 13:37:37 +0700

    init

---
+++ src/contentScript/contentScript.tsx
@@ -0,0 +1,7 @@
+// TODO: content script
+import React from "react";
+import ReactDOM from "react-dom";
+
+const root = document.createElement('div')
+document.body.appendChild(root)
+ReactDOM.render(<h1>qqqqqqqqqqqqqqqqqqqqqqqqqqq</h1>, root)
